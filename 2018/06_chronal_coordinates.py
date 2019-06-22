2b8a1b5b31ba4de53fcf7816a2a485f0f6d1400e
2xRon <smith.byronx@gmail.com>
2019-06-22 16:53:36 -0400

    day 6

--- 2018/06_chronal_coordinates.py
+++ 2018/06_chronal_coordinates.py
@@ -2,23 +2,53 @@
 ADVENT OF CODE: 2018
 Day 6: Chronal Coordinates
 """
-
-in_file = open("inputs/06.input", "r").readlines()
-points = [tuple(map(int, l.replace(" ", "").split(","))) for l in in_file]
-
-max_x = max(points, key=lambda x: x[0])[0]
-max_y = max(points, key=lambda y: y[1])[1]
+from operator import sub, add
+from itertools import product
+from collections import Counter
 
 
-def l1distance(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+def l1distance(c1, c2):
+    return sum(map(abs, map(sub, c1, c2)))  # d = |c1_0 - c2_0| + |c1_1 - c2_1|
 
 
-L1dists = list()
-for yc in range(max_y + 1):
-    xl = list()
-    for xc in range(max_x + 1):
-        xl.append({pt: l1distance(pt, (xc, yc)) for pt in points})
-    L1dists.append(xl)
+in_file = open("inputs/06.input", "r").read().strip()
 
-area_counts = {pt: 0 for pt in points}
+coords = [tuple(map(int, c.split(", "))) for c in in_file.split("\n")]
+n_coords = len(coords)
+dims = len(coords[0])
+
+edges = tuple(map(add, map(max, zip(*coords)), tuple(1 for _ in range(dims))))
+grid = {indices: -1 for indices in product(*map(range, edges))}
+dist_grid = {indices: -1 for indices in product(*map(range, edges))}
+coords_with_edge_neighbor = set()
+
+
+def is_on_edge(coord_index):
+    return any(
+        idx_1 >= idx_2 - 1 or idx_1 == 0 for idx_1, idx_2 in zip(coord_index, edges)
+    )
+
+
+for loc in product(*map(range, edges)):
+    distances = [l1distance(loc, c) for c in coords]
+    dist_grid[loc] = sum(distances)
+    nearest_coord_indexs = [
+        idx for idx, dist in enumerate(distances) if dist == min(distances)
+    ]
+    if len(nearest_coord_indexs) > 1:
+        grid[loc] = None
+    else:
+        grid[loc] = nearest_coord_indexs[0]
+    if is_on_edge(loc):
+        coords_with_edge_neighbor.add(nearest_coord_indexs[0])
+
+nearest_coord_index_counter = Counter(grid.values())
+
+non_edge_areas = sorted(
+    (count, idx)
+    for idx, count in nearest_coord_index_counter.items()
+    if idx not in coords_with_edge_neighbor
+)
+print("Part A:", non_edge_areas[-1][0])
+highly_proximal_locations = [loc for loc, dist in dist_grid.items() if dist < 10000]
+print("Part B:", len(highly_proximal_locations))

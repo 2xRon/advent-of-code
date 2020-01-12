9330eed97146a6388aa9c7656e0bd53dc05fc392
2xRon <smith.byronx@gmail.com>
2020-01-12 12:03:09 -0500

    day 10 fix

--- 2019/10_monitoring_station.py
+++ 2019/10_monitoring_station.py
@@ -20,7 +20,6 @@ def sq_dist(p1, p2):
 
 
 def slope_angle(p1, p2):
-    # atan2 from p1 to p2
     return atan2((p2[1] - p1[1]), (p2[0] - p1[0]))
 
 
@@ -29,8 +28,9 @@ for ast1 in asteroids:
     ast1_angles_to_ast2 = [slope_angle(ast1, ast2) for ast2 in asteroids]
     # colinear stars will have non-unique slope angles, so only count one star from each angle
     visble_asteroids.update({ast1: len(set(ast1_angles_to_ast2))})
-
 print("Part 1: ", visble_asteroids.most_common(1)[0][1])
+
+
 station_addr = visble_asteroids.most_common(1)[0][0]
 station_idx = asteroids.index(station_addr)
 asteroids_by_angle_dict = defaultdict(list)
@@ -46,15 +46,12 @@ living_asteroids_by_angle = [
 ]
 living_asteroids_by_angle.sort(key=lambda x: x[0],)
 
-exploded_asts = 0
 n_angles = len(living_asteroids_by_angle)
+# assume there is at least one asteroid striaght up
 start_angle_index = [x[0] for x in living_asteroids_by_angle].index(-pi / 2)
 angle_idx = start_angle_index
-while True:
+for exploded_asts in range(200):
     if len(living_asteroids_by_angle[angle_idx][1]) > 0:
         last_chopped = living_asteroids_by_angle[angle_idx][1].pop()
-        exploded_asts += 1
-        if exploded_asts == 200:
-            print("Part 2:", last_chopped[0] * 100 + last_chopped[1])
-            break
     angle_idx = (angle_idx + 1) % n_angles
+print("Part 2:", last_chopped[0] * 100 + last_chopped[1])

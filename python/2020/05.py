963d606c5845efcce264f0c815d623fa49898dce
2xRon <smith.byronx@gmail.com>
2020-12-05 13:13:22 -0500

    smol

--- python/2020/05.py
+++ python/2020/05.py
@@ -4,22 +4,10 @@ Day 5: Binary Boarding
 Solution by 2xRon
 """
 
-from typing import Tuple
-
-
-def seat(row_string):
-    return int(
-        "0b"
-        + row_string.replace("F", "0")
-        .replace("B", "1")
-        .replace("L", "0")
-        .replace("R", "1"),
-        2,
-    )
-
+tr = str.maketrans("FBLR", "0101")
 
 with open("./input/05.input") as in_file:
-    seats = set(seat(s) for s in in_file.readlines())
+    seats = set(int(s.translate(tr), 2) for s in in_file.readlines())
     print("Part 1:", max(seats))
-    missing_seat = seats.symmetric_difference(set(range(min(seats), max(seats)+1)))
+    missing_seat = set(range(min(seats), max(seats) + 1)).difference(seats)
     print("Part 2:", tuple(missing_seat)[0])

23be052760ba7d91df4d6d92f06ef8cb88ad51af
2xRon <smith.byronx@gmail.com>
2020-12-06 00:33:42 -0500

    clean up 6

--- python/2020/06.py
+++ python/2020/06.py
@@ -5,11 +5,9 @@ Solution by 2xRon
 """
 
 with open("./input/06.input") as in_file:
-    groups = in_file.read().split("\n\n")
+    groups = [
+        [set(p) for p in g.split("\n")] for g in in_file.read().split("\n\n")
+    ]
 
-yeses = [set(s.replace(" ","").replace("\n","")) for s in groups]
-print("Part 1:", sum(len(y) for y in yeses))
-
-person_groups = [[set(p.strip()) for p in g.split("\n")] for g in groups]
-counts = [len(set.intersection(*g)) for g in person_groups]
-print("Part 2:", sum(counts))
+print("Part 1:", sum(len(set.union(*g)) for g in groups))
+print("Part 2:", sum(len(set.intersection(*g)) for g in groups))
